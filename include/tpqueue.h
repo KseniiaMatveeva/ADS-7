16530789a801adbc2b440f53c57f83adae771498
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-05-02 03:32:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,28 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Item {
+    T value;
+    TItem * next;
+    TItem * prev;
+  };
+  TPQueue :: Item * TRQueue :: creat(const T& value) {
+    Item * item = new Item;
+    item -> value = value;
+    item -> next = nullptr;
+    item -> ptev = nullptr;
+    return item;
+  }
+  Item * head, * tail, * current;
+ public:
+  void push(const T& value) {
+    if (!(head && tail)) {
+      head = tail = creat(value)
+    } else {
+      Item * h = head;
+      Item * v - creat(value);
+      
 };
 
 struct SYM {
